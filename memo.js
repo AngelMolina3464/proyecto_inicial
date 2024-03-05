fd9b53b12d79203a18399c68dff025aca9a5d505
AngelMolina3464 <molina_gap@outlook.es>
2024-03-05 16:25:30 -0600

    Pero con el Repositorio, con el Archivo de Memo

---
+++ memo.js
@@ -0,0 +1,15 @@
+console.log("Git Init ");
+// Para Iniciar el Pro yecto de Git
+
+console.log("git all --all o nombre del Fichero ");
+// Lo agrega para un posible Commit con Cambio de Estatus
+
+console.log('git commit -m "AA1-Msg"');
+// Lanza la Captura de un Commit pero con Mensaje
+
+console.log('"git config --global user.email "molina_gap@outlook.es"');
+//  Es el comando para configurar el Loggin de Git
+// Tambien debe usarse con user.name
+
+console.log('"git driff');
+//  Muestra los Cambio aun no actualizados en el Commit
